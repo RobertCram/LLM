602c06d55e140fead455e928aa3bf6471c447f9e
Robert <robert.cram@gmail.com>
2023-06-05 15:32:55 +0000

    Introduces a form for submitting the data.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import datetime
 import streamlit as st
+from dotenv import load_dotenv
 
-if os.environ["OPENAI_API_KEY"] == "":
-  from dotenv import load_dotenv
-  load_dotenv()
+load_dotenv()
 
 from langchain.llms import OpenAI
 
@@ -20,17 +19,22 @@ Leer 3 bijzonderheden van je voornaam en geboortedatum
 
 """
 
+def generate_answer(name, birthdate):
+  if name == "": return "## Vul een geldige naam in"
+  prompt = f"""Ik ben {name} en geboren op {birthdate.strftime("%d-%m-%Y")}. Kun je 3 interessante bijzonderheden over mijn naam en 3 interessante gebeurtenissen op mijn geboortedatum opnoemen? Formatter het antwoord in markdown en gebruik de headers: Bijzonderheden voornaam: en Bijzonderheden geboortedatum:"""
+  return llm(prompt)
+  
+
+
 ""
-name = "Robert"
-birthdate = datetime.date(1962, 7, 24)
-name = st.text_input("# Voornaam:", value=name, placeholder="Voornaam", max_chars=15)
-birthdate = st.date_input("# Geboortedatum", value=birthdate)
-""
-""
 
-prompt = f"""Ik ben {name} en geboren op {birthdate.strftime("%d-%m-%Y")}. Kun je 3 interessante bijzonderheden over mijn naam en 3 interessante gebeurtenissen op mijn geboortedatum opnoemen? Formatter het antwoord in markdown en gebruik de headers: Bijzonderheden voornaam: en Bijzonderheden geboortedatum:"""
+with st.form("form"):
+  name = st.text_input("# Voornaam:", placeholder="Voornaam", max_chars=15)
+  birthdate = st.date_input("# Geboortedatum", value=datetime.date(2000, 1, 1), min_value=datetime.date(1920, 1, 1))
+  ""
+  ""
+  submitted = st.form_submit_button("Genereer bijzonderheden", use_container_width=True)
 
-st.markdown(llm(prompt))
-
-
+if submitted:
+  st.markdown(generate_answer(name, birthdate))
 
